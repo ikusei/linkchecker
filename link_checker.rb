f6d7a027115c39127f56e591f567a193349c7818
Pyramyd <webmestre@pyramyd.fr>
2011-04-04 11:43:42 +0200

    a bit less ugly

--- link_checker.rb
+++ link_checker.rb
@@ -1,43 +1,88 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'json'
-require 'fastercsv'
+require 'page'
+require 'optparse'
 
 class LinkChecker
- 
-  attr_accessor :urls, :max_urls
-  
-  def initialize(url)
-    self.urls = [url]
+  attr_accessor :pages, :max_urls, :verbose
+  options = {}
+
+  def initialize(url = "http://www.google.com")
+    self.pages = [ Page.new(url) ]
     self.max_urls = 10
   end
- 
+
   def stats
-    self.urls.each do |url|
-      start = Time.now
-      response = open(url)
-      puts "#{url} : #{Time.now - start}s | #{response.status[0]}"
+    self.pages.each_with_index do |page,i|
+      begin
+        start = Time.now
+        response = open(page.url)
+        page.response_code = response.status[0]
+        page.response_time = Time.now - start
+      rescue Exception  => e
+        page.response_code = "404"
+      end
+      puts page
     end
   end
 
   def grab
-    self.urls.each do |url|
-        doc = Nokogiri::HTML(open(url))
-        doc.css('a').each do |link|  
-            store_url("#{url}/#{link['href']}".gsub('http://','http:').gsub('//','/').gsub('http:','http://')) unless link['href'] =~ /^http/
-        end  
+    self.pages.each do |page|
+      doc = Nokogiri::HTML(open(page.url))
+      page.title =  doc.css("title").text
+      page.description =  doc.at('//meta[@name="description"]')['content']
+      doc.css('a').each do |link|  
+        unless link['href'] =~ /^http/
+          if link['href'] && link['href'][0] == 47
+            store_url("#{self.homepage.url}/#{link['href'][/.(.*)/m,1]}",page.url) 
+          else 
+            store_url("#{page.url}/#{link['href']}",page.url) 
+          end
+        end
+      end
     end
-    puts "#{self.urls.count} url(s) grabbed"
   end
-  
+
+  def homepage
+    self.pages.first
+  end
+
   protected
 
-  def store_url(url)
-    if self.urls.size < self.max_urls and !self.urls.include? url
-      puts url
-      self.urls << url
+  def store_url(url,referer)
+    if self.pages.size < self.max_urls and !self.pages.collect {|p| p.url }.include? url
+      self.pages << Page.new(url,referer)
     end
   end
+
+
+
+  ARGV.options do |opts|
+    opts.banner = "Usage: linkchecker [website]"
+    opts.on("-m", "--max [MAX]", "Maximum of links to follow", "Default: 10") do |max|
+      options['max'] =  max
+    end
+
+
+    opts.on("-v", "--verbose", "Outputs some useless informations") do |v|
+      options['verbose'] = true
+    end
+
+    opts.on( '-h', '--help', 'Display this screen' ) do
+      puts opts
+      exit
+    end
+
+    opts.parse!
+    checker = LinkChecker.new(ARGV.pop)
+    checker.verbose = true if options['verbose']
+    checker.max_urls = options['max'].to_i
+    checker.grab
+    checker.stats
+  end
+
 
 end
--- page.rb
+++ page.rb
@@ -2,20 +2,25 @@ require 'digest/md5'
 require 'rainbow'
 
 class Page
-  attr_accessor :url, :response_time, :response_code, :title, :meta, :referer
+  attr_accessor :url, :response_time, :response_code, :title, :meta, :referer, :description, :verbose
   
-  def initialize(url,referer = "No Referer")
+  def initialize(url,referer = "No Referer",verbose = false)
     self.url=url
     self.response_time  = 0
     self.response_code = 0
     self.title = "No title"
-    self.referer = referer 
+    self.description = "No description"
+    self.referer = referer
+    self.verbose = verbose 
   end
   
   def to_s
     response_code = self.response_code != "200" ? self.response_code.to_s.color(:red) : self.response_code.to_s.color(:green)
     title =  self.title == "No title" ? self.title.color(:red) : self.title
-    "#{self.url} | #{self.referer} | #{self.response_time}s | #{response_code} | #{title}"
+    
+    output = "#{self.url}"
+    output += " | #{self.referer}" if self.verbose
+    output += " | #{self.response_time}s | #{response_code} | #{title} | #{description}"
   end
   
  
